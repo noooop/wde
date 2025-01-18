74c73dfb8356b07af6fd2901c4a919da421e066f
noooop <noooop@126.com>
2025-01-18 18:41:32 +0800

    [Releases] v0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="wde",
-    version="v0.2.3",
+    version="v0.3.0",
     author="noooop",
     author_email="noooop@live.cn",
     description="Workflow Defined Engine",
