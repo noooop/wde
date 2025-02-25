0ed567896cde446085c27e8aec60bb0caddb996b
noooop <noooop@126.com>
2025-02-25 16:47:06 +0800

    [core] Lazy load QUANTIZATION_METHODS

--- wde/workflows/core/backends/quantization/__init__.py
+++ wde/workflows/core/backends/quantization/__init__.py
@@ -1,49 +1,44 @@
 from typing import Dict, Type
 
-from wde.workflows.core.backends.quantization.aqlm import AQLMConfig
-from wde.workflows.core.backends.quantization.awq import AWQConfig
-from wde.workflows.core.backends.quantization.awq_marlin import AWQMarlinConfig
+from wde.utils import LazyLoader, lazy_import
 from wde.workflows.core.backends.quantization.base_config import \
     QuantizationConfig
-from wde.workflows.core.backends.quantization.bitsandbytes import \
-    BitsAndBytesConfig
-from wde.workflows.core.backends.quantization.deepspeedfp import \
-    DeepSpeedFPConfig
-from wde.workflows.core.backends.quantization.fbgemm_fp8 import FBGEMMFp8Config
-from wde.workflows.core.backends.quantization.fp8 import Fp8Config
-from wde.workflows.core.backends.quantization.gptq import GPTQConfig
-from wde.workflows.core.backends.quantization.gptq_marlin import \
-    GPTQMarlinConfig
-from wde.workflows.core.backends.quantization.gptq_marlin_24 import \
-    GPTQMarlin24Config
-from wde.workflows.core.backends.quantization.marlin import MarlinConfig
-from wde.workflows.core.backends.quantization.qqq import QQQConfig
-from wde.workflows.core.backends.quantization.squeezellm import \
-    SqueezeLLMConfig
 
-QUANTIZATION_METHODS: Dict[str, Type[QuantizationConfig]] = {
-    "aqlm": AQLMConfig,
-    "awq": AWQConfig,
-    "deepspeedfp": DeepSpeedFPConfig,
-    "fp8": Fp8Config,
-    "fbgemm_fp8": FBGEMMFp8Config,
+_QUANTIZATION_METHODS: Dict[str, str] = {
+    "aqlm": "wde.workflows.core.backends.quantization.aqlm:AQLMConfig",
+    "awq": "wde.workflows.core.backends.quantization.awq:AWQConfig",
+    "deepspeedfp":
+    "wde.workflows.core.backends.quantization.deepspeedfp:DeepSpeedFPConfig",
+    "fp8": "wde.workflows.core.backends.quantization.fp8:Fp8Config",
+    "fbgemm_fp8":
+    "wde.workflows.core.backends.quantization.fbgemm_fp8:FBGEMMFp8Config",
     # The order of gptq methods is important for config.py iteration over
     # override_quantization_method(..)
-    "marlin": MarlinConfig,
-    "gptq_marlin_24": GPTQMarlin24Config,
-    "gptq_marlin": GPTQMarlinConfig,
-    "awq_marlin": AWQMarlinConfig,
-    "gptq": GPTQConfig,
-    "squeezellm": SqueezeLLMConfig,
-    "bitsandbytes": BitsAndBytesConfig,
-    "qqq": QQQConfig,
+    "marlin": "wde.workflows.core.backends.quantization.marlin:MarlinConfig",
+    "gptq_marlin_24":
+    "wde.workflows.core.backends.quantization.gptq_marlin_24:GPTQMarlin24Config",
+    "gptq_marlin":
+    "wde.workflows.core.backends.quantization.gptq_marlin:GPTQMarlinConfig",
+    "awq_marlin":
+    "wde.workflows.core.backends.quantization.awq_marlin:AWQMarlinConfig",
+    "gptq": "wde.workflows.core.backends.quantization.gptq:GPTQConfig",
+    "squeezellm":
+    "wde.workflows.core.backends.quantization.squeezellm:SqueezeLLMConfig",
+    "bitsandbytes":
+    "wde.workflows.core.backends.quantization.bitsandbytes:BitsAndBytesConfig",
+    "qqq": "wde.workflows.core.backends.quantization.qqq:QQQConfig",
+}
+
+QUANTIZATION_METHODS: Dict[str, Type[QuantizationConfig]] = {
+    _name: LazyLoader(_module)
+    for _name, _module in _QUANTIZATION_METHODS.items()
 }
 
 
 def get_quantization_config(quantization: str) -> Type[QuantizationConfig]:
-    if quantization not in QUANTIZATION_METHODS:
+    if quantization not in _QUANTIZATION_METHODS:
         raise ValueError(f"Invalid quantization method: {quantization}")
-    return QUANTIZATION_METHODS[quantization]
+    return lazy_import(_QUANTIZATION_METHODS[quantization])
 
 
 __all__ = [
